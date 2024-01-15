885ebff0aaa58b75268c6a37d134384675677e8e
HeppyFeet <leogomescom91@gmail.com>
2024-01-15 14:24:54 -0300

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+
+    <!--Script de adicionar icons-->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <!--Script de adicionar bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+
+    <title>@yield('title')</title>
+
+
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+@section('title')
+@section('content')
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <td>Caixa Aberto</td>
+            </div>
+
+        </div>
+    </div>
+@endsection
